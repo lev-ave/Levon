d1a1110e4fdab923c579b2f282080b7c1f543ba7
Levon Avetisyan <levon.avetisyan.2009@gmail.com>
2025-09-20 23:25:35 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// let number = prompt('random number')
+// if(number > 0){
+//     alert ("1")
+// }
+// else if(number < 0){
+//     alert ("-1")
+// }
+// else{
+//     alert ("0")
+// }
+
+
+// let result = (a * b < 4) ? 'Мало' : 'Много';
+
+
+// for(let i=0; i<=40; i+=2){
+//     console.log(i);
+// }
+
+
+// let number = 1;
+// for(let i=1; i<=30; i++){
+//     number *= i;
+// }
+// console.log(number);
+
+
+// let result = 0;
+// let number = 1;
+// while(number<=40){
+//     if(number%2==0){
+//         result += number;
+//     }
+//     number++;
+// }
+// console.log(result);
+
+
+// for(let i=0; i<=40; i+=7){
+//     console.log(i);
+// }
+
+
+// let result = 1;
+// for(let i=1; i<=60; i++){
+//     if(i%6==0){
+//         result *= i;
+//     }
+// }
+// console.log(result);
