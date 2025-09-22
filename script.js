90a492d589ff719a04272a5cfc3302da935d7a20
Levon Avetisyan <levonavetisyan@Levons-MacBook-Air.local>
2025-09-22 23:19:18 +0400

    function exercises

--- script.js
+++ script.js
@@ -1,50 +1,76 @@
-// let number = prompt('random number')
-// if(number > 0){
-//     alert ("1")
-// }
-// else if(number < 0){
-//     alert ("-1")
-// }
-// else{
-//     alert ("0")
-// }
-
-
-// let result = (a * b < 4) ? 'Мало' : 'Много';
-
-
-// for(let i=0; i<=40; i+=2){
-//     console.log(i);
-// }
-
-
-// let number = 1;
-// for(let i=1; i<=30; i++){
-//     number *= i;
-// }
-// console.log(number);
-
-
-// let result = 0;
-// let number = 1;
-// while(number<=40){
-//     if(number%2==0){
-//         result += number;
+// function checkDivisibleBy3(number){
+//     if(number % 3 == 0){
+//         console.log(number + "patik e 3-i")
 //     }
-//     number++;
-// }
-// console.log(result);
-
-
-// for(let i=0; i<=40; i+=7){
-//     console.log(i);
-// }
-
-
-// let result = 1;
-// for(let i=1; i<=60; i++){
-//     if(i%6==0){
-//         result *= i;
+//     else{
+//         console.log(number + "3-i patik che")
 //     }
 // }
-// console.log(result);
+// checkDivisibleBy3(7)
+
+
+// function maxOfTwo(a, b){
+//     if(a > b){
+//         return a;
+//     }
+//     else{
+//         return b;
+//     }
+// }
+// console.log(maxOfTwo(5, 4))
+
+
+// function getSum(number){
+//     let result = 0;
+//     for(let i=1; i<number; i++){
+//         result += i;
+//     }
+//     return result;
+// }
+// console.log(getSum(7))
+
+
+// function getTimes(number){
+//     let result = 1;
+//     for(let i=1; i<number; i++){
+//         if(i % 2 == 1){
+//             result *= i;
+//         }
+//     }
+//     return result;
+// }
+// console.log(getTimes(10))
+
+
+// function getGrades(number){
+//     if(number == 10 || number == 9){
+//         return "gerazanc";
+//     }
+//     else if(number == 8 || number == 7){
+//         return "lav";
+//     }
+//     else if(number <= 6){
+//         return "bavarar";
+//     }
+//     }
+//     console.log(getGrades(5)) 
+
+
+// function minOfThree(a, b, c){
+//     if(a < b && a < c){
+//         return a;
+//     }
+//     else if(b < a && b < c){
+//         return b;
+//     }
+//     else if(c < a && c < b){
+//         return c;
+//     }
+// }
+// console.log(minOfThree(9, 5, 6))
+
+
+// function raisedToThePower(a, b){
+//     return a**b;
+// }
+// console.log(raisedToThePower(3, 4))
