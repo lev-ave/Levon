246e823affab0fc96d0e65c141ea0b6eed7a8b58
Levon Avetisyan <levonavetisyan@Levons-MacBook-Air.local>
2025-09-29 20:49:14 +0400

    homework

---
+++ script.js
@@ -0,0 +1,53 @@
+// let arr = [78, 26, 52, 3, 1, 77]
+// arr.push(123)
+// console.log(arr)
+// arr.unshift(74, 52)
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+
+
+// const numbers = [3, 7, 10, 15, 20, 25]
+// let biggerThan10 = numbers.find(number => number > 10)
+// console.log(biggerThan10)
+// let firstDivisibleBy5 = numbers.find(number => number % 5 === 0)
+// console.log(firstDivisibleBy5)
+
+
+// const numbers = [5, 12, 8, 130, 44]
+// let biggerThan10 = numbers.filter(number => number > 10)
+// console.log(biggerThan10)
+// let evenNumbers = numbers.filter(number => number % 2 === 0)
+// console.log(evenNumbers)
+
+
+// const words = ["spray", "limit", "elite", "exuberant", "destruction", "present"]
+// let longerThan6 = words.filter(word => word.length > 6)
+// console.log(longerThan6)
+
+
+// const numbers = [1, 2, 3, 4, 5].map(number => number * 2)
+// console.log(numbers)
+// const numberss = [1, 2, 3, 4, 5].map(number => number + 10)
+// console.log(numberss)
+
+
+// const people = [
+// { name: "Anna", age: 17 },
+// { name: "Karen", age: 22 },
+// { name: "Aram", age: 15 },
+// { name: "Mery", age: 19 }
+// ]
+// let over18YearsOld = people.find(people => people.age > 18)
+// console.log(over18YearsOld)
+// let adult = people.filter(people => people.age > 18)
+// console.log(adult)
+// const peopleNames = [
+// { name: "Anna", age: 17 },
+// { name: "Karen", age: 22 },
+// { name: "Aram", age: 15 },
+// { name: "Mery", age: 19 }
+// ].map(peopleName => peopleName.name)
+// console.log(peopleNames)
