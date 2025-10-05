4a9d03f31a7831434d3b7a2c3fbbef106df199a1
Levon Avetisyan <levonavetisyan@Levons-MacBook-Air.local>
2025-10-05 19:04:35 +0400

    homework

---
+++ script.js
@@ -0,0 +1,60 @@
+// let arr = [3,2,4,7,6]
+// arr.splice(2, 1)
+// console.log(arr)
+
+
+// let arr = [78, 29, 66, 40, 17, 1]
+// arr.splice(0, 3, 33, 99, 55)
+// console.log(arr)
+
+
+// function connectAndReverse(arr1, arr2){
+//     let a = arr1.concat(arr2)
+//     let b = a.reverse()
+//     return b;
+// }
+// let c = [9, 8, 7]
+// let d = [3, 2, 1]
+// let result = connectAndReverse(c, d)
+// console.log(result)
+
+
+// let string = "яблоко,банан,киви,груша"
+// let arr = string.split(`,`)
+// console.log(arr)
+
+
+// let arr = ["Я", "учу", "JS"]
+// let string = arr.join(` `)
+// console.log(string)
+
+
+// let arr = ["html", "css", "js"]
+// console.log(arr.includes("js"))
+
+
+// function twoString(arr, string){
+//     return arr.includes(string)
+// }
+// let names = ["Levon", "John", "Max", "Vicky"]
+// console.log(twoString(names, "Levon"))
+
+
+// let arr = [10, 20, 30, 40, 50]
+// arr.splice(0, 3)
+// console.log(arr)
+
+
+// let arr = ["red", "green", "blue", "yellow"]
+// arr.splice(2,1, "purple")
+// console.log(arr)
+
+
+// let string = "Привет как дела"
+// let a = string.split(" ").reverse().join(" ")
+// console.log(a)
+
+
+// let arr = ["cat", "dog", "hamster", "parrot"]
+// let result = arr.filter(word => word.includes("r"))
+// console.log(result)
